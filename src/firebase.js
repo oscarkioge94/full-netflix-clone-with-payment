fda23c08ca07df30334cc41e63ceb1c477766e7d
unknown <mainaoscar94@gmail.com>
2021-02-16 10:43:42 +0300

    started work on my app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyALXzq_6MYrPxnrTX5suCKM8QGtLgN2kUw",
+  authDomain: "netflixx-full-clon.firebaseapp.com",
+  projectId: "netflixx-full-clon",
+  storageBucket: "netflixx-full-clon.appspot.com",
+  messagingSenderId: "368577806680",
+  appId: "1:368577806680:web:e81a44f247b0772e44e52d",
+};

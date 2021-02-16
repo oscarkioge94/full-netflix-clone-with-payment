948d3d0acf3617542900dca04e3992a08de13961
unknown <mainaoscar94@gmail.com>
2021-02-16 11:06:01 +0300

    working on the navbar

---
+++ src/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './HomeScreen.css'
+import Nav from './Nav'
+
+function HomeScreen() {
+    return (
+        <div className="homescreen">
+            {/* nav */}
+            <Nav/>
+            {/* banner */}
+            {/* row */}
+            
+        </div>
+    )
+}
+
+export default HomeScreen

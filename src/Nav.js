d3c21213479cdc6e29275b31f005c8a006818cc3
unknown <mainaoscar94@gmail.com>
2021-02-16 11:36:25 +0300

    implimenting show and hide functionality in navbar

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,21 @@ import './Nav.css'
 
 function Nav() {
     return (
-        <div className="nav">
+        <div className="nav nav__black">
+            <div className="nav__content">
+            <img
+            className="nav__logo"
+            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            alt="logo"
+            />
+            <img
+            className="nav__avatar"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="avatar"
+            />
+            </div>
+            
+
             
         </div>
     )
